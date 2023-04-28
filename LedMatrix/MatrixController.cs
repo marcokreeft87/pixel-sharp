003c1150a6f68aea78253416a4b183b64384d7b3
Marco Kreeft <marco.kreeft@gmail.com>
2023-04-28 11:46:04 +0200

    Cleanup

--- LedMatrix/MatrixController.cs
+++ LedMatrix/MatrixController.cs
@@ -21,20 +21,10 @@ public class MatrixController : Controller
     [HttpGet("matrix/render")]
     public async Task<string> RenderImage([FromQuery] string imageUrl) 
     {
-        // Initialize the LED matrix display
-        var options = new RGBLedMatrixOptions() 
-        {
-            Rows = _ledRows,
-            Cols = _ledColumns,
-            ChainLength = 1,
-            Parallel = 1,
-            GpioSlowdown = 4,
-        };
-
-        var matrix = new RGBLedMatrix(options);
+        var matrix = GetMatrix();
         var canvas = matrix.CreateOffscreenCanvas();
         canvas.Clear();
-        
+
         var bitmap = await BitmapHelper.GetBitmap(imageUrl, _ledColumns, _ledRows);
 
         // Loop through the pixels of the image and set the corresponding pixel in the RGBLedMatrix canvas
@@ -56,5 +46,46 @@ public class MatrixController : Controller
         canvas = matrix.SwapOnVsync(canvas);
         
         return "Image rendered";
+    }
+
+    [HttpGet("matrix/text")]
+    public void ScrollText([FromQuery] string text) 
+    {
+        var matrix = GetMatrix();
+        
+        text = "This is a scrolling text.";
+
+        var canvas = matrix.CreateOffscreenCanvas();
+        var font = new RGBLedFont("fonts/6x10.bdf");
+
+        canvas.DrawText(font, 1, 6, new Color(0, 255, 0), text);
+        
+        matrix.SwapOnVsync(canvas);
+        // var graphics = new rpi_rgb_led_matrix_sharp.Graphics(matrix);
+        // var font = new Font("Arial", 16, FontStyle.Bold);
+        // var brush = new SolidBrush(Color.Red);
+
+
+        // while (true)
+        // {
+        //     graphics.Clear();
+        //     graphics.DrawText(text, font, brush, 0, 0, true);
+        //     matrix.SwapOnVSync();
+        // }
+    }
+
+    private RGBLedMatrix GetMatrix()
+    {
+        // Initialize the LED matrix display
+        var options = new RGBLedMatrixOptions() 
+        {
+            Rows = _ledRows,
+            Cols = _ledColumns,
+            ChainLength = 1,
+            Parallel = 1,
+            GpioSlowdown = 4,
+        };
+
+        return new RGBLedMatrix(options);        
     }
 }
